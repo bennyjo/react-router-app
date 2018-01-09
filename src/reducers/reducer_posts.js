8ae80ada47beb255d79193edbd180d71cf67bb58
Benny Johansson <benny.json@gmail.com>
2018-01-09 17:45:40 +0000

    feat(reducer_posts): handle the DELETE_POST action

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
 // Object descructing
-import { FETCH_POSTS, FETCH_POST } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions/index';
 
-export default function(state = {}, action) {
+export default function(posts = {}, action) {
   switch(action.type) {
+    case DELETE_POST:
+      const { [action.payload]: id, ...otherPosts } = posts;
+
+      return otherPosts;
     case FETCH_POST:
       // Nested object destructing
       const { payload: { data } } = action;
@@ -11,12 +15,12 @@ export default function(state = {}, action) {
   
       // Spread syntax using the spread operator
       // Computed property names
-      return { ...state, [post.id]: post };
+      return { ...posts, [post.id]: post };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
-      return state;
+      return posts;
   }
 
-  return state;
+  return posts;
 }
