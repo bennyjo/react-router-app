c67ae069b1f18945b9dcfda01b694559a4214f7a
Benny Johansson <benny.json@gmail.com>
2018-01-09 17:45:40 +0000

    feat(fetch_posts): add action

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions/index';
+// Object descructing
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 
 export default function(state = {}, action) {
+  // Nested object destructing
+  const { payload: { data } } = action;
+
   switch(action.type) {
     case FETCH_POSTS:
-      return _.mapKeys(action.payload.data, 'id');
+      const post = data;
+      // Spread syntax using the spread operator
+      // Computed property names
+      return { ...state, [data.id]: data };
+    case FETCH_POSTS:
+      return _.mapKeys(data, 'id');
     default:
       return state;
   }
