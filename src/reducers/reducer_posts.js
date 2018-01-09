f80ea89e15d3d714149dfb56250702cfb80e7425
Benny Johansson <benny.json@gmail.com>
2018-01-09 17:45:40 +0000

    fix(reducer_posts): correct file name and default return value

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
 import { FETCH_POSTS } from '../actions/index';
 
-export default function(state, action) {
+export default function(state = [], action) {
   switch(action.type) {
     case FETCH_POSTS:
-      return _mapKeys(action.payload.data, 'id');
+      return _.mapKeys(action.payload.data, 'id');
     default:
       return state;
   }
+
+  return state;
 }
