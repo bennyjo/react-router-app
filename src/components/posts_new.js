465b017ad978eb607afb9de555295b95a0abbc70
Benny Johansson <benny.json@gmail.com>
2018-01-09 17:45:40 +0000

    feat(posts_new): add component

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Posts extends Component {
+  render() {
+    return (
+      <div>
+        Posts new!
+      </div>
+    )
+  }
+}
+
+export default Posts;
